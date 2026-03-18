9be4ecdbdd7748fae4e9d55ac300b302bcc4b477
master-sauce <idanhb17@gmail.com>
2026-03-18 15:14:12 +0200

    Create start_windows.bat

---
+++ dvora-web/dvora-dist/start_windows.bat
@@ -0,0 +1,16 @@
+@echo off
+TITLE DVORA - Media Scanner
+echo Starting DVORA...
+
+REM Start the Go application in the background
+start "DVORA Server" /MIN go run main.go
+
+REM Wait a moment for the server to start
+timeout /t 2 /nobreak >nul
+
+REM Open the default browser
+start http://localhost:8080
+
+echo DVORA is now running at http://localhost:8080
+echo Press any key to close this window (server will keep running)...
+pause >nul
