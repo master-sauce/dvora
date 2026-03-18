715143e90679a0ebec64099c11ea16452fc6952e
master-sauce <idanhb17@gmail.com>
2026-03-18 15:31:35 +0200

    better

--- dvora-web/dvora-dist/start_windows.bat
+++ dvora-web/dvora-dist/start_windows.bat
@@ -6,11 +6,11 @@ REM Start the Go application in the background
 start "DVORA Server" /MIN go run main.go
 
 REM Wait a moment for the server to start
-timeout /t 4 /nobreak >nul
+timeout /t 2 /nobreak >nul
 
 REM Open the default browser
 start http://localhost:8080
 
 echo DVORA is now running at http://localhost:8080
-echo Press any key to close this window (server will keep running)...
-pause >nul
+timeout /t 1 /nobreak >nul
+exit
