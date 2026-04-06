011cfe9a61c5005a5b3b84b9f4066b0e3abee06c
master-sauce <idanhb17@gmail.com>
2026-04-07 01:04:04 +0300

    added /search /filter in skip list in web

--- dvora-web/dvora-dist/main.go
+++ dvora-web/dvora-dist/main.go
@@ -213,7 +213,7 @@ func scanSite(siteURL, searchTerm string) (bool, string, []LogEntry, error) {
 
 	skip := []string{"addtoany.com", "facebook.com", "twitter.com", "reddit.com",
 		"pinterest.com", "whatsapp.com", "t.me", "mailto:", "/login", "/register",
-		"/signup", "/feed", "#"}
+		"/signup", "/feed", "#", "/filter", "/search"}
 
 	var matched []string
 	skipped := 0
